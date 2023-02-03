c173d80b6768990a9ea80c057a5da841390b332d
Dinithi Malmida <112160974+DinithiMalmi@users.noreply.github.com>
2023-02-03 15:03:39 +0530

    Activities

--- app/src/main/java/com/example/aipoweredsnakedetectionsystem/SnakeListActivity.java
+++ app/src/main/java/com/example/aipoweredsnakedetectionsystem/SnakeListActivity.java
@@ -1,14 +1,29 @@
 package com.example.aipoweredsnakedetectionsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SnakeListActivity extends AppCompatActivity {
+
+    Button btnSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snake_list);
+        btnSecond = findViewById(R.id.btnSecond);
+
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SnakeListActivity.this, CameraActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
